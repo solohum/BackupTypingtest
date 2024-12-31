78e0ebd6a470919f882c7b5293a621016eaf3240
solohum <sarthaksarthak6001@gmail.com>
2024-12-31 21:43:21 +0530

    Create sql.py

---
+++ sql.py
@@ -0,0 +1,12 @@
+import mysql.connector 
+
+
+import mysql.connector
+
+myconn = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword")
+
+cursor = myconn.cursor()
+cursor.execute(create database if not exists Typing test
